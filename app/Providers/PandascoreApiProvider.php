de76508043964e59d59f4605631f8a13709e5c9a
Ildar Saribzhanov <ildar.r.saribzhanov@gmail.com>
2020-06-29 20:35:36 +0300

    [Add] pandas api package

---
+++ app/Providers/PandascoreApiProvider.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use PandaScoreAPI\PandaScoreAPI;
+
+/**
+ * Class PandascoreApiProvider
+ *
+ * @package App\Providers
+ */
+class PandascoreApiProvider extends ServiceProvider
+{
+    /** @var bool */
+    protected $defer = true;
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->singleton(PandaScoreAPI::class, function () {
+            return new PandaScoreAPI([
+                PandaScoreAPI::SET_TOKEN => env('PANDASCORE_TOKEN'),
+            ]);
+        });
+    }
+
+    public function provides()
+    {
+        return [PandaScoreAPI::class];
+    }
+}
