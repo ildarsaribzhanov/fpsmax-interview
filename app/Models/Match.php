d48e56ab5053ed8a4f5085a2e00d656acff7a64b
Ildar Saribzhanov <ildar.r.saribzhanov@gmail.com>
2020-06-30 16:10:05 +0300

    [Update] model attributes

--- app/Models/Match.php
+++ app/Models/Match.php
@@ -18,5 +18,11 @@ use Illuminate\Database\Eloquent\Model;
  */
 class Match extends Model
 {
-    //
+    protected $table = 'matches';
+
+    protected $fillable = [
+        'ext_id',
+        'begin_at',
+        'name',
+    ];
 }
